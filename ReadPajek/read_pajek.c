7a74fae92a60f01c39f623c4a02b45e2b0868989
Gustavo Barbosa <gustavosb64@gmail.com>
2021-06-24 21:37:16 -0300

    Editando read_pajek.c para ignorar strings NULL

--- ReadPajek/read_pajek.c
+++ ReadPajek/read_pajek.c
@@ -49,11 +49,19 @@ void readPajek(Graph *G, FILE *file){
     int Mi, Mj;
     char *str1, *str2;
     while(!feof(file)){
-    
+
         str1 = readline(file);
+        if (str1 == NULL){
+            free(str1); 
+            break;
+        }
         Mi = atoi(str1);
 
         str2 = readline(file);
+        if (str2 == NULL){
+            free(str2); 
+            break;
+        }
         Mj = atoi(str2);
         
         free(str1);
